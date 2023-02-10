d262db0434978da574aea009c289d5afe6cfcc8d
chioni <chioni1620@gmail.com>
2023-02-10 12:18:34 +0530

    Setup Interrupts
    Load Interrupt Table

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
 
+mod interrupts;
 mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn rust_start() -> ! {
+    interrupts::init();
     println!("Hello again!");
     println!("some numbers: {}", 42);
     loop {}
